6042e94140cd7b43d6d792d769068a9c045a4b49
InsomniaROM <codekill18@gmail.com>
2013-10-10 17:24:33 -0400

    Update codekill_d2spr.mk

--- products/codekill_d2spr.mk
+++ products/codekill_d2spr.mk
@@ -41,5 +41,5 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/codekill/overlay/720p
 
 # Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += \
+#PRODUCT_PACKAGE_OVERLAYS += \
     vendor/codekill/overlay/hot_reboot
